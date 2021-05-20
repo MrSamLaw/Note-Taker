811047fee361213d41c25cbd8895d4faa374f399
Mr Sam Law <sam@chongland.com>
2021-05-20 22:09:12 +1000

    Catch-All route re-enabled

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,7 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    app.get('/index', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
