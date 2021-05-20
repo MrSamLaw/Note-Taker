c3b759e7f1a41cc040b4d300aecf1adb24071444
Mr Sam Law <sam@chongland.com>
2021-05-20 13:40:20 +1000

    / route implemented

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,9 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
