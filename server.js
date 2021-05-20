712bee37b562cd71925b2ab4aed0032e23974a3e
Mr Sam Law <sam@chongland.com>
2021-05-20 22:07:01 +1000

    Rearranged Routing file calls

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
