11a981afd11e27d268e6cf8f43b3cdf9e28b824b
Mr Sam Law <sam@chongland.com>
2021-05-20 14:12:26 +1000

    Fixed routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
     app.get('/index', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
